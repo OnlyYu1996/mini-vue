b8ed7d0a8f2a50cb02f837654f8bb4f26051dfc7
OnlyYu1996 <1158673577@qq.com>
2023-06-15 22:38:35 +0800

    12-优化 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,4 +1,7 @@
 import { extend } from '../shared';
+
+let activeEffect;
+let shouldTrack = false;
 class ReactiveEffect {
   private _fn: any;
   deps = [];
@@ -8,8 +11,17 @@ class ReactiveEffect {
     this._fn = fn;
   }
   run() {
+    if (!this.active) {
+      return this._fn();
+    }
+    // 应该收集
+    shouldTrack = true;
     activeEffect = this;
-    return this._fn();
+    const result = this._fn();
+
+    // 重置
+    shouldTrack = false;
+    return result;
   }
   stop() {
     if (this.active) {
@@ -24,13 +36,13 @@ function cleanUpEffect(effect) {
   effect.deps.forEach((dep: any) => {
     dep.delete(effect);
   });
+  effect.deps.length = 0;
 }
-
-let activeEffect;
 
 const targetMap = new Map();
 
 export function track(target, key) {
+  if (!isTracking()) return;
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -41,7 +53,7 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
-  if (!activeEffect) return;
+  if (dep.has(activeEffect)) return;
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
 }
@@ -54,6 +66,10 @@ export function effect(fn, options: any = {}) {
   const runner: any = _effect.run.bind(_effect);
   runner.effect = _effect;
   return runner;
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
